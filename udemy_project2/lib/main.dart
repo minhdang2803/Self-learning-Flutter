feeaac7c9da074c97ba297b722e12c6af2cc4b84
minhdang2803 <58287394+minhdang2803@users.noreply.github.com>
2021-09-19 23:08:02 +0700

    New change in appBarTheme

--- udemy_project2/lib/main.dart
+++ udemy_project2/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:udemy_project2/models/transaction.dart';
 import 'package:udemy_project2/widgets/transaction_list.dart';
 import './widgets/new_transaction.dart';
+import './widgets/charts.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,27 +13,20 @@ class MyApp extends StatelessWidget {
       title: 'Personal Expenses',
       home: MyHomePage(),
       theme: ThemeData(
-        primarySwatch: Colors.purple,
-        fontFamily: 'Quicksand',
-        //TextTheme: Setting theme for the rest of the application
-        textTheme: ThemeData.light().textTheme.copyWith(
-              headline1: TextStyle(
-                  fontFamily: 'OpenSans',
-                  fontSize: 17,
-                  fontWeight: FontWeight.bold),
-            ),
-        //appBarTheme: Setting theme for the appBar of the application
-        appBarTheme: AppBarTheme(
-          //default appbar theme
+          primarySwatch: Colors.purple,
+          fontFamily: 'Quicksand',
+          //TextTheme: Setting theme for the rest of the application
           textTheme: ThemeData.light().textTheme.copyWith(
                 headline6: TextStyle(
-                    // big Text for appbar Text
                     fontFamily: 'OpenSans',
-                    fontSize: 20,
+                    fontSize: 15,
                     fontWeight: FontWeight.bold),
               ),
-        ),
-      ),
+          appBarTheme: ThemeData.light().appBarTheme.copyWith(
+              titleTextStyle: TextStyle(
+                  fontFamily: 'OpenSans',
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold))),
     );
   }
 }
@@ -44,6 +38,21 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final List<Transaction> _userTransaction = [];
+
+  List<Transaction> get getRecentTransactions {
+    return _userTransaction
+        .where((element) =>
+            element.date.isAfter(
+              DateTime.now().subtract(
+                Duration(days: 7),
+              ),
+            ) &&
+            element.date.isBefore(
+              DateTime.now().subtract(Duration(hours: 24)),
+            ))
+        .toList(); // because where( () => ()) return an Iterable so we need to cast .toList();
+  }
+
   void _addNewTransaction(String txtitle, double txAmount) {
     final newTransaction = Transaction(
       id: DateTime.now().toString(),
@@ -71,12 +80,8 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          'Personal Expenses',
-          style: TextStyle(
-            fontFamily: 'OpenSans',
-          ),
-        ),
+        title: Text('Personal Expenses',
+            style: Theme.of(context).appBarTheme.titleTextStyle),
         actions: <Widget>[
           IconButton(
             onPressed: () => _startAddNewTransaction(context),
@@ -89,18 +94,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Container(
-              width: double.infinity,
-              child: Card(
-                child: Text(
-                  "Chart!",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                ),
-                elevation: 5,
-                color: Colors.blue[100],
-              ),
-            ),
+            Charts(getRecentTransactions),
             TransactionList(_userTransaction)
           ],
         ),
